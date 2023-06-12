3e02c9852657056e80db6c792bb52ea993761fcd
Sohag Rana <sohagrana921@gmail.com>
2023-06-12 21:27:53 +0600

    card responsive done

--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -12,14 +12,6 @@ const useAxiosSecure = () => {
   const navigate = useNavigate();
 
   useEffect(() => {
-    // axiosSecure.interceptors.request.use((config) => {
-    //   const token = localStorage.getItem("access-token");
-    //   if (token) {
-    //     config.headers.Authorization = `Bearer ${token}`;
-    //   }
-    //   return config;
-    // });
-
     axiosSecure.interceptors.response.use(
       (response) => response,
       async (error) => {
